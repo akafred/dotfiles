c7ecabd0358de356a09be11bc8c80855d5e37128
akafred <github@akafred.com>
2020-12-03 13:07:52 +0100

    New install proc for homebrew

--- .provisioning/bootstrap.sh
+++ .provisioning/bootstrap.sh
@@ -7,7 +7,7 @@ sudo xcode-select --install || true
 
 # Install Homebrew
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew doctor
 
 # Install Ansible
