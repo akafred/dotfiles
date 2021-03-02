9301f9c870ebfa1cd506ad3849fdcf11008d931a
akafred <github@akafred.com>
2021-03-02 17:33:26 +0100

    Minor adjustments to bootstrap.sh

--- .provisioning/bootstrap.sh
+++ .provisioning/bootstrap.sh
@@ -6,15 +6,22 @@
 sudo xcode-select --install || true
 
 # Install Homebrew
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# you might need:
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+. $HOME/.zprofile
+
+# check with
 brew doctor
 
 # Install Ansible
-
 brew install ansible
 
-# Install requirements froom Ansible Galaxy
-ansible-galaxy install -r requirements.yml
+# Make sure you are in .provisioning folder
+cd .provisioning
+
+# Install requirements from Ansible Galaxy
+ansible-galaxy install -r requirements.yml  
 
 # You should now be ready to run ansible locally.
