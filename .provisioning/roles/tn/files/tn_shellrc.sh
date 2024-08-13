bfc2d3a238c173c19fc43fae5c7f4616d3ef4912
akafred <github@akafred.com>
2024-08-13 16:59:27 +0200

    Justeringer for tn

---
+++ .provisioning/roles/tn/files/tn_shellrc.sh
@@ -0,0 +1,7 @@
+
+# så lenge man er i riktig context kan man f.eks. kjøre port_forward helse-spion-frontend og nå den fra localhost:8080
+#port_forward() {
+#  TARGET=$(echo $(kubectl get pods | grep "^$1-" -m 1 | cut -d' ' -f1))
+#  CONTAINER_PORT=$(kubectl get pod $TARGET --template='{{(index (index .spec.containers 0).ports 0).containerPort}}{{"\n"}}')
+#  kubectl port-forward pods/$TARGET 8080:$CONTAINER_PORT
+#}
